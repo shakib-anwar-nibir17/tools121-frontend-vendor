465f1d2e1d1ea38dcc05eb9b80478604bae27d5b
shakib-anwar-nibir17 <nibirshakib@gmail.com>
2024-05-19 11:50:04 +0600

    vendor dashborad demo

--- app/(vendorDashboard)/test/page.jsx
+++ app/(vendorDashboard)/test/page.jsx
@@ -1,49 +1,7 @@
 import { VendorProfileSVG } from "@/components/Icons/Icons";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import ProfileTabs from "@/components/vendor/ProfileTabs";
-
-const vendorProfile = [
-  {
-    value: "dashboard",
-    key: "Dashboard",
-  },
-  {
-    value: "profile",
-    key: "Profile",
-  },
-  {
-    value: "manage_products",
-    key: "Manage Products",
-  },
-  {
-    value: "quotation",
-    key: "Quotation Request",
-  },
-  {
-    value: "manage_order",
-    key: "Manage Order",
-  },
-  {
-    value: "subscriptions",
-    key: "Subscriptions",
-  },
-  {
-    value: "advertisement",
-    key: "Advertisement",
-  },
-  {
-    value: "seller_page",
-    key: "Seller Page",
-  },
-  {
-    value: "password",
-    key: "Password",
-  },
-  {
-    value: "system_preference",
-    key: "System Preference",
-  },
-];
+import { vendorProfile } from "@/lib/tabOptions";
 
 const TestPage = () => {
   return (
--- components/vendor/ProfileTabs.jsx
+++ components/vendor/ProfileTabs.jsx
@@ -1,37 +1,24 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import VendorProfile from "@/components/vendor/VendorProfile";
 import ProfileContent from "@/components/vendor/contents/ProfileContent";
+import { profileOptions } from "@/lib/tabOptions";
 
 const ProfileTabs = () => {
+  console.log(profileOptions);
   return (
     <Tabs defaultValue="profile">
       <div className="border border-slate-200 px-6 pt-4 py-3 rounded-xl box-border shadow-md">
         <VendorProfile />
         <TabsList className="space-x-6 justify-between mt-12 font-bold text-primary-950 p-0">
-          <TabsTrigger
-            className="text-lg pb-0 data-[state=active]:bg-white data-[state=active]:text-primary-950 data-[state=active]:border-b-4  data-[state=active]:border-primary-900 hover:border-b-4 hover:border-primary-900"
-            value="overview"
-          >
-            <span className="py-4">Overview</span>
-          </TabsTrigger>
-          <TabsTrigger
-            className="text-lg pb-0 data-[state=active]:bg-white data-[state=active]:text-primary-950 data-[state=active]:border-b-4  data-[state=active]:border-primary-900 hover:border-b-4 hover:border-primary-900"
-            value="profile"
-          >
-            <span className="py-4">Profile</span>
-          </TabsTrigger>
-          <TabsTrigger
-            className="text-lg pb-0 data-[state=active]:bg-white data-[state=active]:text-primary-950 data-[state=active]:border-b-4  data-[state=active]:border-primary-900 hover:border-b-4 hover:border-primary-900"
-            value="verify"
-          >
-            <span className="py-4">Verify Shop</span>
-          </TabsTrigger>
-          <TabsTrigger
-            className="text-lg pb-0 data-[state=active]:bg-white data-[state=active]:text-primary-950 data-[state=active]:border-b-4  data-[state=active]:border-primary-900 hover:border-b-4 hover:border-primary-900"
-            value="reviews"
-          >
-            <span className="py-4">Reviews</span>
-          </TabsTrigger>
+          {profileOptions.map((option) => (
+            <TabsTrigger
+              key={option.value} // Remember to add a unique key when mapping over components in React
+              className="text-lg pb-0 data-[state=active]:bg-white data-[state=active]:text-primary-950 data-[state=active]:border-b-4 data-[state=active]:border-primary-900 hover:border-b-4 hover:border-primary-900"
+              value={option.value}
+            >
+              <span className="py-4">{option.key}</span>
+            </TabsTrigger>
+          ))}
         </TabsList>
       </div>
       <div>
