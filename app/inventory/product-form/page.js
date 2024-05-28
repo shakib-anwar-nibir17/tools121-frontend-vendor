7867223292f33f287574cd569acdc06d365bcd38
shakib-anwar-nibir17 <nibirshakib@gmail.com>
2024-05-28 10:30:04 +0600

    minor fix 2

--- app/inventory/product-form/page.js
+++ app/inventory/product-form/page.js
@@ -1,6 +1,7 @@
 import GeneralInfo from "@/components/Dashboard/ProductRequest/GeneralInfo";
 import MediaInfo from "@/components/Dashboard/ProductRequest/MediaInfo";
 import MoreInfo from "@/components/Dashboard/ProductRequest/MoreInfo";
+import { Button } from "@/components/ui/button";
 
 const ProductRequestForm = () => {
   return (
@@ -12,6 +13,14 @@ const ProductRequestForm = () => {
         <MediaInfo />
         {/* More Information */}
         <MoreInfo />
+        <div className="mt-12 mb-[50px]">
+          <div className="flex justify-end gap-4">
+            <Button className="text-xl px-6 bg-white text-primary-900 border border-primary-900">
+              Cancel
+            </Button>
+            <Button className="text-xl px-6">Save Products</Button>
+          </div>
+        </div>
       </form>
     </div>
   );
