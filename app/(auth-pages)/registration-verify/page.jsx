8e7ee76fcfd46bb2f5aabab0c28b96ab2a85f172
shakib-anwar-nibir17 <nibirshakib@gmail.com>
2024-06-25 12:22:51 +0600

    validation and data reading of auth related forms

--- app/(auth-pages)/registration-verify/page.jsx
+++ app/(auth-pages)/registration-verify/page.jsx
@@ -1,58 +1,112 @@
 "use client";
 import { useRegisterOtpVerifyMutation } from "@/app/redux/features/authApi";
-import OTPTimer from "@/components/common/OTPTimer";
 import { Button } from "@/components/ui/button";
 import {
   InputOTP,
   InputOTPGroup,
   InputOTPSlot,
 } from "@/components/ui/input-otp";
+import { useEffect, useState } from "react";
 import { useGoogleReCaptcha } from "react-google-recaptcha-v3";
-import toast from "react-hot-toast";
 import { useSelector } from "react-redux";
 
 export default function Verify() {
   const { executeRecaptcha } = useGoogleReCaptcha();
-  const registerdata = useSelector((state) => state.authStore.registerdata)
-  const [verifyRegOtp, { }] = useRegisterOtpVerifyMutation();
-  
+  const registerdata = useSelector((state) => state.authStore.registerdata);
+  const [verifyRegOtp, {}] = useRegisterOtpVerifyMutation();
+  const [otpValue, setOtpValue] = useState(null);
+  const [error, setError] = useState();
+
+  const [countDown, setCountDown] = useState(59);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (countDown > 0) {
+        setCountDown(countDown - 1);
+      }
+      if (countDown === 0) {
+        clearInterval(interval);
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [countDown]);
+
   const variFicationHandler = async () => {
     const token = await executeRecaptcha("verify_otp");
     const request_Obj = {
       login_name: registerdata?.login_name,
       recaptcha_token: token,
-      otp: "571877"
-    }
-    const verifyRes = await verifyRegOtp(request_Obj)
+      otp: "571877",
+    };
+    const verifyRes = await verifyRegOtp(request_Obj);
 
-    console.log("VerifyRes ===>", verifyRes)
-  }
+    console.log("VerifyRes ===>", verifyRes);
+  };
+
+  const resendOTP = () => {
+    document.getElementById("otpForm").reset();
+    setCountDown(59);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (otpValue === null) {
+      setError(
+        "Please Provide your 4 digit OTP number! Do not share this with anyone"
+      );
+    } else {
+      if (otpValue.length < 4) {
+        setError("OTP must have 4 digits");
+      } else {
+        console.log(otpValue);
+
+        setError(null);
+      }
+    }
+  };
+
   return (
-    <>
+    <div className="w-[448px]">
       <div className="pb-11 text-center lg:text-left">
         <h1 className=" text-2xl sm:text-3xl lg:text-4xl text-[#01060D] font-bold pb-3">
-          Verify Account
+          Enter your OTP
         </h1>
-        <p className="mt-3 text-xl">Verify OTP to confirm your registration.</p>
+        <p className="mt-3 text-xl">Enter OTP to confirm your verification.</p>
       </div>
-      <InputOTP maxLength={4}>
-        <InputOTPGroup className="h-16 w-[448px]">
-          <InputOTPSlot index={0} />
-          <InputOTPSlot index={1} />
-          <InputOTPSlot index={2} />
-          <InputOTPSlot index={3} />
-        </InputOTPGroup>
-      </InputOTP>
-      <p className="pt-2 text-black text-lg">
-        Enter the code sent to your mobile.
-      </p>
-      <button onClick={variFicationHandler}  className="text-white h-16 w-full mt-10 rounded-2xl text-xl bg-primary-900">Verify</button>
+      <form id="otpForm" onSubmit={handleSubmit}>
+        <InputOTP onChange={(value) => setOtpValue(value)} maxLength={4}>
+          <InputOTPGroup className="h-16 w-[448px]">
+            <InputOTPSlot index={0} />
+            <InputOTPSlot index={1} />
+            <InputOTPSlot index={2} />
+            <InputOTPSlot index={3} />
+          </InputOTPGroup>
+        </InputOTP>
+        <p className="pt-2 text-black text-lg">
+          Enter the code sent to your mobile.
+        </p>
+        <p className="pt-2 text-red-500 text-lg">{error}</p>
 
-      {/* <Button  className="h-16 w-full mt-10 rounded-2xl text-xl">Verify</Button> */}
-
+        <Button
+          type="submit"
+          onClick={variFicationHandler}
+          className="h-16 mt-10 rounded-2xl text-xl w-[448px]"
+        >
+          Verify
+        </Button>
+      </form>
       <p className="pt-4 text-black text-lg text-center">
-        Haven’t received it? Resend it after - <OTPTimer  />
+        Haven’t received it? Resend it after -{" "}
+        <span className="font-bold">{countDown}s</span>
+        {countDown === 0 && (
+          <button onClick={resendOTP} className="text-primary-900 ml-3">
+            Resend OTP
+          </button>
+        )}
       </p>
-    </>
+    </div>
   );
 }
