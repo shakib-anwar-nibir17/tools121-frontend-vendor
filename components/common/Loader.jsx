ab801e19c7031d3b5f7683aae4d4e28602506cad
shakib-anwar-nibir17 <nibirshakib@gmail.com>
2024-07-10 12:44:19 +0600

    loading component added

---
+++ components/common/Loader.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/prop-types */
+const Loader = () => {
+  return (
+    <div className="w-20 h-20 animate-[spin_1s_linear_infinite] rounded-full border-4 border-r-transparent border-l-transparent border-sky-400"></div>
+  );
+};
+
+export default Loader;
