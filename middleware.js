5c6d454084bb8e59ce7f878c929f21a31de8c194
shakib-anwar-nibir17 <nibirshakib@gmail.com>
2024-07-18 09:54:04 +0600

    private route implimentation

--- middleware.js
+++ middleware.js
@@ -1,17 +1,23 @@
+import { LOGIN, PUBLIC_ROUTES, ROOT } from "@/utils/routes";
 import { NextResponse } from "next/server";
-
 export function middleware(req) {
+  const { nextUrl } = req;
+  const token = req.cookies.get("vendorToken");
+
   const { pathname } = req.nextUrl;
 
-  const redirectPattern = /^\/(store-settings(\/profile-settings\/?)?)?$/;
+  const isPublicRoute =
+    PUBLIC_ROUTES.find((route) => nextUrl.pathname.startsWith(route)) ||
+    nextUrl.pathname === ROOT;
+  console.log("Current pathname:", pathname);
+  console.log({ isPublicRoute });
 
-  if (
-    redirectPattern.test(pathname) ||
-    pathname === "/store-settings/profile-settings"
-  ) {
-    return NextResponse.redirect(new URL("/dashboard", req.url));
-  }
+  if (!token && !isPublicRoute)
+    return Response.redirect(new URL(LOGIN, nextUrl));
 
-  // Allow the request to proceed
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
