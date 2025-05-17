c20cfaee03f5e979d4d181ca607da7669b391234
Shakib Anwar Nibir <nibirshakib@gmail.com>
2025-05-17 11:41:29 +0600

    style: update logo link in Header and SiteLogo components to point to dashboard; adjust logo dimensions for consistency

--- components/common/SiteLogo.jsx
+++ components/common/SiteLogo.jsx
@@ -4,14 +4,14 @@ import Link from "next/link";
 const SiteLogo = () => {
   return (
     <div className="absolute md:px-16 px-4 2xl:py-10 xl:py-6 py-4">
-      <Link href={process.env.NEXT_PUBLIC_WEBSITE_URL}>
+      <Link href={"/"}>
         <Image
-          className="2xl:w-48 xl:h-12 lg:w-40 w-36 h-8"
+          className="md:w-full md:h-full"
           src="/logo.png"
           alt="logo"
           priority={false}
-          width={197}
-          height={48}
+          width={219}
+          height={54}
         />
       </Link>
     </div>
