170a7664b4a321bf1c93220d070b4b6f1a0907d7
Shakib Anwar Nibir <nibirshakib@gmail.com>
2025-05-18 14:00:43 +0600

    style: refactor TrendingItemCard component for improved readability; adjust image objectFit property

--- components/common/TrendingItemCard.jsx
+++ components/common/TrendingItemCard.jsx
@@ -1,24 +1,22 @@
 import { BASE_URL } from "@/constant/urls";
 import Image from "next/image";
 
-const TrendingItemCard = ({product}) => {
-  
+const TrendingItemCard = ({ product }) => {
   return (
     <div className="">
-      <div className="relative w-[180px] h-[120px] rounded-lg border border-2 border-gray-200">
-    
-          <Image
+      <div className="relative w-[180px] h-[120px] rounded-lg  border-2 border-gray-200">
+        <Image
           layout="fill"
-          objectFit="cover"
+          objectFit="contain"
           src={`${BASE_URL}/generate-file/?file_path=${product?.img_url}`}
           // src={generateFile(product?.img_url)}
           alt="top_trending_item"
         />
       </div>
-      <h1 className="text-black mt-4 text-sm mb-8">
-       {product?.name}
-      </h1>
-      <p className="text-black text-lg text-center font-medium">{product?.quote_cnt} pieces</p>
+      <h1 className="text-black mt-4 text-sm mb-8">{product?.name}</h1>
+      <p className="text-black text-lg text-center font-medium">
+        {product?.quote_cnt} pieces
+      </p>
     </div>
   );
 };
